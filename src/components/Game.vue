8e11dd8ea67bb887f21aef32426331c81023dc73
etothepi16 <paulmcdowell18@gmail.com>
2019-08-23 16:25:49 -0400

    restructured

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,13 +1,63 @@
 <template>
-
+<div id="game">
+    <h1>Cash Reserves: ${{ cash }}</h1>
+    <br>
+    <h1>Bake Sales: {{ bakeSales }}</h1>
+    <button v-on:click="getCash">Make it rain!</button>
+    <button v-on:click="bakeSale">Run a bake sale!</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Game',
-  props: {
+ data(){
+    return {
+      //Resources and buildings go here
+      //TODO: You must construct additional pylons! (make some buildings, etc)
+      salary: 1,
+      cash: 0,
+      bakeSales: 0,
+      yardSales: null,
+      marketStalls: null,
+      onlineStores: null,
+      youTubeChannels: null,
+      viralMarketingCampaigns: null,
+      interns: null,
+      employees: null,
+      llcs: null,
+      brandDeals: null,
+      stockPrice:null,
+      shares:null,
+      subsidiaries:null,
+      contractors: null
+    }
+  },
+  methods:{
+    //ALL YOUR FUNCTIONS ARE BELONG TO US
+    earnSalary: function(){
+      this.cash += this.salary;
+    },
+    getCash: function(){
+      this.cash++;
+    },
+    bakeSale: function(){
+      this.bakeSales += 1;
+      this.cash += (10*this.bakeSales);
+    },
+  //Gameloop
+    gameLoop: function(){
+      this.bakeSale();
+      //Other building methods once implemented
+      //TODO: implement more building-related methods
+      //TODO: refactor to have a building manager
+    },
+    mounted: function(){
+      setInterval(this.gameLoop, 1000);
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,8 @@
 <template>
-  <div id="app">
-    <h1>Cash Reserves: ${{ cash }}</h1>
-    <br>
-    <h1>Bake Sales: {{ bakeSales }}</h1>
-    <button v-on:click="getCash">Make it rain!</button>
-    <button v-on:click="bakeSale">Run a bake sale!</button>
+  <div>
+    <Game />
   </div>
+  
 </template>
 
 <script>
@@ -14,51 +11,6 @@ export default {
   name: 'Home',
   components: {
     Game
-  },
-  data(){
-    return {
-      //Resources and buildings go here
-      //TODO: You must construct additional pylons! (make some buildings, etc)
-      salary: 1,
-      cash: 0,
-      bakeSales: 0,
-      yardSales: null,
-      marketStalls: null,
-      onlineStores: null,
-      youTubeChannels: null,
-      viralMarketingCampaigns: null,
-      interns: null,
-      employees: null,
-      llcs: null,
-      brandDeals: null,
-      stockPrice:null,
-      shares:null,
-      subsidiaries:null,
-      contractors: null
-    }
-  },
-  methods:{
-    //ALL YOUR FUNCTIONS ARE BELONG TO US
-    earnSalary: function(){
-      this.cash += this.salary;
-    },
-    getCash: function(){
-      this.cash++;
-    },
-    bakeSale: function(){
-      this.bakeSales += 1;
-      this.cash += (10*this.bakeSales);
-    },
-  //Gameloop
-    gameLoop: function(){
-      this.bakeSale();
-      //Other building methods once implemented
-      //TODO: implement more building-related methods
-      //TODO: refactor to have a building manager
-    },
-    mounted: function(){
-      setInterval(this.gameLoop, 1000);
-    }
   }
 }
 </script>
@@ -70,7 +22,7 @@ export default {
   margin:0;
   padding:0;
 }
-#app {
+#game {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
